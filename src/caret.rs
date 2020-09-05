971a9ef673b3698b3f1fc166024d2385d2f97fe0
Alula <julekonopinska@gmail.com>
2020-09-05 06:43:14 +0200

    more carets

--- src/caret.rs
+++ src/caret.rs
@@ -1,14 +1,7 @@
 use crate::bitfield;
-use crate::common::{Direction, Rect};
+use crate::common::{Condition, Direction, Rect};
 use crate::engine_constants::EngineConstants;
 use crate::rng::RNG;
-
-bitfield! {
-  pub struct Cond(u16);
-  impl Debug;
-
-  pub visible, set_visible: 7;
-}
 
 #[derive(Debug, EnumIter, PartialEq, Eq, Hash, Copy, Clone)]
 pub enum CaretType {
@@ -40,11 +33,11 @@ pub struct Caret {
     pub vel_y: isize,
     pub offset_x: isize,
     pub offset_y: isize,
-    pub cond: Cond,
-    pub direct: Direction,
+    pub cond: Condition,
+    pub direction: Direction,
     pub anim_rect: Rect<usize>,
     anim_num: usize,
-    anim_wait: isize,
+    anim_counter: isize,
 }
 
 impl Caret {
@@ -58,11 +51,11 @@ impl Caret {
             vel_y: 0,
             offset_x,
             offset_y,
-            cond: Cond(0x80),
-            direct,
+            cond: Condition(0x80),
+            direction: direct,
             anim_rect: Rect::<usize>::new(0, 0, 0, 0),
             anim_num: 0,
-            anim_wait: 0,
+            anim_counter: 0,
         }
     }
 
@@ -74,40 +67,67 @@ impl Caret {
             CaretType::Shoot => {}
             CaretType::SnakeAfterimage | CaretType::SnakeAfterimage2 => { // dupe, unused
             }
-            CaretType::Zzz => {}
+            CaretType::Zzz => {
+                if self.anim_counter == 0 {
+                    self.anim_rect = constants.caret.zzz_rects[self.anim_num];
+                }
+
+                self.anim_counter += 1;
+                if self.anim_counter > 4 {
+                    self.anim_counter = 0;
+                    self.anim_num += 1;
+                }
+
+                if self.anim_num == constants.caret.zzz_rects.len() {
+                    self.cond.set_alive(false);
+                }
+
+                self.x += 0x80; // 0.4fix9
+                self.y -= 0x80;
+            }
             CaretType::Exhaust => {
-                self.anim_wait += 1;
-                if self.anim_wait > 1 {
-                    self.anim_wait = 0;
+                self.anim_counter += 1;
+                if self.anim_counter > 1 {
+                    self.anim_counter = 0;
                     self.anim_num += 1;
                 }
 
                 if self.anim_num >= constants.caret.exhaust_rects.len() {
-                    self.cond.set_visible(false);
+                    self.cond.set_alive(false);
                     return;
                 }
 
                 self.anim_rect = constants.caret.exhaust_rects[self.anim_num];
 
-                match self.direct {
+                match self.direction {
                     Direction::Left => { self.x -= 0x400; } // 2.0fix9
                     Direction::Up => { self.y -= 0x400; }
                     Direction::Right => { self.x += 0x400; }
                     Direction::Bottom => { self.y += 0x400; }
                 }
             }
-            CaretType::DrownedQuote => {}
+            CaretType::DrownedQuote => {
+                if self.anim_counter == 0 {
+                    self.anim_counter = 1;
+
+                    match self.direction {
+                        Direction::Left => { self.anim_rect = constants.caret.drowned_quote_left_rect; }
+                        Direction::Right => { self.anim_rect = constants.caret.drowned_quote_right_rect; }
+                        _ => {}
+                    }
+                }
+            }
             CaretType::QuestionMark => {
-                self.anim_wait += 1;
-                if self.anim_wait < 5 {
+                self.anim_counter += 1;
+                if self.anim_counter < 5 {
                     self.y -= 0x800; // 4.0fix9
                 }
 
-                if self.anim_wait == 32 {
-                    self.cond.set_visible(false);
+                if self.anim_counter == 32 {
+                    self.cond.set_alive(false);
                 }
 
-                self.anim_rect = match self.direct {
+                self.anim_rect = match self.direction {
                     Direction::Left => { constants.caret.question_left_rect }
                     Direction::Right => { constants.caret.question_right_rect }
                     _ => { self.anim_rect }
@@ -118,7 +138,7 @@ impl Caret {
             CaretType::Explosion => {}
             CaretType::LittleParticles => {
                 if self.anim_num == 0 {
-                    match self.direct {
+                    match self.direction {
                         Direction::Left => {
                             self.vel_x = rng.range(-0x300..0x300) as isize; // -1.5fix9..1.5fix9
                             self.vel_y = rng.range(-0x100..0x100) as isize; // -0.5fix9..0.5fix9
@@ -132,7 +152,7 @@ impl Caret {
 
                 self.anim_num += 1;
 
-                if self.direct == Direction::Left {
+                if self.direction == Direction::Left {
                     self.vel_x = (self.vel_x * 4) / 5;
                     self.vel_y = (self.vel_y * 4) / 5;
                 }
@@ -141,19 +161,19 @@ impl Caret {
                 self.y += self.vel_y;
 
                 if self.anim_num == 20 {
-                    self.cond.set_visible(false);
+                    self.cond.set_alive(false);
                     return;
                 }
 
                 self.anim_rect = constants.caret.little_particles_rects[self.anim_num / 2 % constants.caret.little_particles_rects.len()];
 
-                if self.direct == Direction::Right {
+                if self.direction == Direction::Right {
                     self.x -= 4 * 0x200;
                 }
             }
             CaretType::Unknown => {
                 // not implemented because it was apparently broken in og game?
-                self.cond.set_visible(false);
+                self.cond.set_alive(false);
             }
             CaretType::SmallProjectileDissipation => {}
             CaretType::Empty => {}
@@ -161,7 +181,8 @@ impl Caret {
         }
     }
 
+    #[inline]
     pub fn is_dead(&self) -> bool {
-        !self.cond.visible()
+        !self.cond.alive()
     }
 }
