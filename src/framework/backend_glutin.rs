f99b452073b8ff877be5a657283e27375e91fb39
biroder <107300789+biroder@users.noreply.github.com>
2023-04-01 20:27:30 +0300

    Fix sound volume change and inventory overflow on Android (#207)
    * Android changing volume fix. Added arrows to indicate volume buttons
    
    * Fix inventory overflowing on Android

--- src/framework/backend_glutin.rs
+++ src/framework/backend_glutin.rs
@@ -133,7 +133,11 @@ fn get_insets() -> GameResult<(f32, f32, f32, f32)> {
         vm_env.get_int_array_region(field, 0, &mut elements)?;
 
         vm_env.delete_local_ref(JObject::from_raw(field));
-
+        
+        //Game always runs with horizontal orientation so top and bottom cutouts not needed and only wastes piece of the screen
+        elements[1] = 0;
+        elements[3] = 0;
+        
         Ok((elements[0] as f32, elements[1] as f32, elements[2] as f32, elements[3] as f32))
     }
 }
