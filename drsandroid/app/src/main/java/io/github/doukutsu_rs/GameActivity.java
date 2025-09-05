0caa8f4376502ba510d2e3045c64fa65dc9bb57a
Alula <git@alula.me>
2025-09-05 23:33:22 +0200

    Modernize the Java code

--- drsandroid/app/src/main/java/io/github/doukutsu_rs/GameActivity.java
+++ drsandroid/app/src/main/java/io/github/doukutsu_rs/GameActivity.java
@@ -1,21 +1,19 @@
 package io.github.doukutsu_rs;
 
+import static android.os.Build.VERSION.SDK_INT;
+
 import android.app.NativeActivity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.hardware.SensorManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.view.OrientationEventListener;
-import android.view.WindowInsets;
 import android.widget.Toast;
 
 import java.io.File;
-
-import static android.os.Build.VERSION.SDK_INT;
 
 public class GameActivity extends NativeActivity {
     private int[] displayInsets = new int[]{0, 0, 0, 0};
@@ -72,7 +70,7 @@ public class GameActivity extends NativeActivity {
         this.displayInsets[2] = 0;
         this.displayInsets[3] = 0;
 
-        WindowInsets insets = getWindow().getDecorView().getRootWindowInsets();
+        var insets = getWindow().getDecorView().getRootWindowInsets();
 
         if (insets != null) {
             this.displayInsets[0] = Math.max(this.displayInsets[0], insets.getStableInsetLeft());
@@ -84,7 +82,7 @@ public class GameActivity extends NativeActivity {
         }
 
         if (SDK_INT >= Build.VERSION_CODES.P) {
-            android.view.DisplayCutout cutout = insets.getDisplayCutout();
+            var cutout = insets.getDisplayCutout();
 
             if (cutout != null) {
                 this.displayInsets[0] = Math.max(this.displayInsets[0], cutout.getSafeInsetLeft());
@@ -96,22 +94,22 @@ public class GameActivity extends NativeActivity {
     }
 
     public void openDir(String path) {
-        Uri uri = DocumentsContract.buildDocumentUri(BuildConfig.DOCUMENTS_AUTHORITY, path);
+        var uri = DocumentsContract.buildDocumentUri(BuildConfig.DOCUMENTS_AUTHORITY, path);
 
-        File file = new File(path);
+        var file = new File(path);
         if (!file.isDirectory()) {
             Toast.makeText(getApplicationContext(), R.string.dir_not_found, Toast.LENGTH_LONG).show();
             return;
         }
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+        var intent = new Intent(Intent.ACTION_VIEW);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setDataAndType(uri, DocumentsContract.Document.MIME_TYPE_DIR);
         intent.setFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION | Intent.FLAG_GRANT_PREFIX_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
         try {
             startActivity(intent);
-        } catch(ActivityNotFoundException e) {
+        } catch (ActivityNotFoundException e) {
             Toast.makeText(getApplicationContext(), R.string.no_app_found_to_open_dir, Toast.LENGTH_LONG).show();
         }
     }
